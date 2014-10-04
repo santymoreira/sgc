c848500244eb456aa38ef26f6797314f6308cf1f
Jhairo Fc <jhairorivera91@hotmail.com>
2014-10-03 20:31:47 -0500

    sdsd

--- app/views/home/layout.blade.php
+++ app/views/home/layout.blade.php
@@ -96,7 +96,7 @@
                               <li>{{ HTML::link('finanzas/finanzas_sgc', 'Escuela de Ingeniería Financiera'); }}</li>
                               <li>{{ HTML::link('marketing/marketing_sgc', 'Escuela de Ingeniería en Marketing'); }}</li>
                               <li>{{ HTML::link('transporte/transporte_sgc', 'Escuela de Ingeniería en Gestión de Transporte'); }}</li>
-                           
+                             
                           
                              
 										</ul>
