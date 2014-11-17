957d8136b8b7423a26c8eec51fc6f3c954c1a10c
Paul <santymoreira@gmail.com>
2014-11-17 17:05:25 -0500

    modified:   app/views/empleados/busquedaBalance.blade.php 	modified:   app/views/reportes/empleadosFotosBalance.blade.php 	modified:   public/images/70.PNG

--- app/views/reportes/empleadosFotosBalance.blade.php
+++ app/views/reportes/empleadosFotosBalance.blade.php
@@ -1,38 +1,15 @@
 
   {{ HTML::script('js/jquery-1.8.2.min.js'); }}
-<style>
-.display_box /*estilos para cada caja unitaria de cada usuario que se muestra*/
-{
-padding:2px;
-padding-left:6px; 
-font-size:18px;
-height:63px;
-color:#3b5999; 
-}
-
-.display_box:hover /*estilos para cada caja unitaria de cada usuario que se muestra. cuando el mause se pocisiona sobre el area*/
-{
-background: #7f93bc;
-color: #FFF;
-}
-
-</style>
 
 
-<div id="resu"></div>
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <input type="hidden" id="esc" value="{{ $escuela }}">
 <input type="hidden" id="tipoReporte" value="{{ $tipoReporte }}">
-
- 
- <div id="contenido">
+<div id="contenido">
   @foreach ($empleados as $empleado)
 <a id="{{$empleado->COD_EMPLEADO}}"> 
   <div id="displayBox" align="left" >
         <img src="{{ asset('images/fotoreal.jpg'); }}" width="30" height="30" />
         <font color="#0B0B61"><b>{{$empleado->NOMBRES}}</b></font>
-          <br/><br/>
         <input type="hidden" id="{{$empleado->COD_EMPLEADO}}ci" value="{{$empleado->CI}}">
         <input type="hidden" id="{{$empleado->COD_EMPLEADO}}nombres" value="{{$empleado->NOMBRES}}">
         <input type="hidden" id="{{$empleado->COD_EMPLEADO}}mail" value="{{$empleado->EMAIL}}">
